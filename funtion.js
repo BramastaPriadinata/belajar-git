24b0818091415397e356efb69270ef964af75880
BramastaPriadinata <bramastap35@gamil.com>
2023-04-01 11:57:20 +0700

    perbaikan fungsi perkalian

--- funtion.js
+++ funtion.js
@@ -4,9 +4,8 @@ console.log(add(1,2));
 const substract=(a,b)=>a-b;
 console.log(substract(5,3));
 
-const multiply=(a,b)=>a*b;
-console.log(multiply(2,3));
-
+const multiplyNumber=(a,b)=>a*b;
+console.log(multiplyNumber(2,3));
 
 const divide=(a,b) => a/b;
 console.log(divide(6,2));
